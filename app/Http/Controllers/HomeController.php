6403ac6110c92341ddc3a64a855862280efb08ee
yebrai <ivansevv@gmail.com>
2024-02-21 19:26:08 +0100

    added controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function __invoke() //Solo administra una unica ruta
+    {
+        return "Bienvenido a la pagina principal";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,22 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return "test";
-});
+Route::get('/', HomeController::class);
 
-Route::get('cursos', function () {
-    return "welcomeewerwe";
-});
+Route::get('cursos', [CursoController::class, 'index']);
 
-Route::get('users/create', function () {
-    return "Creacion de cursos";
-});
+Route::get('cursos/create',[CursoController::class, 'create']);
 
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if ($categoria) {
-        return "Bienvenido al curso: $curso, de la categoría $categoria";
-    } else {
-        return "Bienvenido al curso: $curso";
-    }
-});
+Route::get('cursos/{cursos}', [CursoController::class, 'show']);
+
+// Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+//     if ($categoria) {
+//         return "Bienvenido al curso: $curso, de la categoría $categoria";
+//     } else {
+//         return "Bienvenido al curso: $curso";
+//     }
+// });
