79d0ebcad53fb8b793a09a4ab5a3105542c8fd2e
yebrai <ivansevv@gmail.com>
2024-02-22 22:05:57 +0100

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id();//Integrer Unsigned Increment
+            $table->string('name');//varchar 255
+            // $table->text('comentarios'); // More chars
+            $table->string('email')->unique();//
+            $table->timestamp('email_verified_at')->nullable();// nullable for probably empty
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
+            $table->timestamps();// created_at updated_at
         });
     }
 
